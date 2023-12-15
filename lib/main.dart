3fac6d489a6af5be3ef4b9c3ed971c8bde37ea02
hae8064 <hae8064@naver.com>
2023-12-15 22:30:12 +0900

    Init: first project init

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+// Widget은 레고 블럭이라고 생각 하면 된다.
+// Widget이 합쳐져서 앱이 만들어진다. 컴포넌트라고 생각하자
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+// CupertinoApp: IOS
+// MateiralApp: 안드로이드
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello Flutter'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text(
+            'Hello Flutter!',
+          ),
+        ),
+      ),
+    );
+  }
+}
