ba424df76d4c5b23ffffd45d644bcdb78dcef420
hae8064 <hae8064@naver.com>
2023-12-19 23:04:03 +0900

    design: 좌우 padding, 배경색, 글자색 변경

--- lib/main.dart
+++ lib/main.dart
@@ -9,21 +9,47 @@ void main() {
 // Widget은 레고 블럭이라고 생각 하면 된다.
 // Widget이 합쳐져서 앱이 만들어진다. 컴포넌트라고 생각하자
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
 // CupertinoApp: IOS
 // MateiralApp: 안드로이드
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Flutter'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Text(
-            'Hello Flutter!',
+        // 색깔 뒤 shade는 채도 조절 가능
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Hey, Selena",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        "Welcome back",
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
           ),
         ),
       ),
