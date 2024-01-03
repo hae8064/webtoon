af96e8ee1dfd219753a2dec6e37e248dfe9816e2
hae8064 <hae8064@naver.com>
2024-01-03 23:36:33 +0900

    feat: 카드 컴포넌트 개발 및 아이콘 위치 조정, overflow기능

--- lib/main.dart
+++ lib/main.dart
@@ -87,20 +87,99 @@ class MainApp extends StatelessWidget {
                       text: 'Request',
                       bgColor: Color(0xFF1F2123),
                       textColor: Colors.white),
-                  // Padding(
-                  //   padding: EdgeInsets.symmetric(
-                  //     vertical: 20,
-                  //     horizontal: 50,
-                  //   ),
-                  //   child: Text(
-                  //     'Transfer',
-                  //     style: TextStyle(
-                  //       fontSize: 16,
-                  //     ),
-                  //   ),
-                  // ),
                 ],
               ),
+              const SizedBox(
+                height: 100,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Text(
+                    'Wallets',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 36,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    'View All',
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.8),
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                // overflow 생겼을 때 이벤트 처리 가능
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF1F2123),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Euro",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 32,
+                                fontWeight: FontWeight.w600),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              const Text(
+                                "6 428",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                "EUR",
+                                style: TextStyle(
+                                  color: Colors.white.withOpacity(0.7),
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Transform.scale(
+                        scale: 2.4,
+                        child: Transform.translate(
+                          // offset은 요소의 위치를 초과해서 옮김
+                          offset: const Offset(-5, 10),
+                          child: const Icon(
+                            Icons.euro_rounded,
+                            color: Colors.white,
+                            size: 78,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
